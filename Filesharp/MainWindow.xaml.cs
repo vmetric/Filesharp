87f9f8314e3355a60cb0f1057d94fab3d9fb6bde
Matt <40653288+vmetric@users.noreply.github.com>
2018-08-30 13:04:30 -0600

    Complete crude move function
    Completed a crude and non-recursive move function. Potential memory and/or performance issues.

--- Filesharp/MainWindow.xaml.cs
+++ Filesharp/MainWindow.xaml.cs
@@ -30,34 +30,29 @@ namespace Filesharp
             control.Visibility = Visibility.Visible;
         }
 
-        public void startMove(string sourceDir, string destDir, string filetype)
+        public void startMove(string sourceDirectory, string destDirectory, string filetype)
         {
-            textbox1.Text = "Source directory (e.g., C:\\1\\)";
-            textbox2.Text = "Destination directory (e.g., C:\\2\\";
-            textbox3.Text = "Filetype to move (e.g., .png)";
+            int filesMoved = 0;
 
-            hideControl(textbox4);
-            
-                // NOTE TO SELF: directory.getfiles SHOULD WORK!
-                // Look into it!
-                string[] filesToMove = Directory.GetFiles(sourceDir, "*" + filetype);
-                try
+            DirectoryInfo sourceDir = new DirectoryInfo(@sourceDirectory);
+            FileInfo[] filesToMove = sourceDir.GetFiles("*" + filetype);
+
+            try
+            {
+                foreach (FileInfo fileToMove in filesToMove)
                 {
-                    MessageBox.Show(filesToMove[1]);
+                    File.Move(sourceDirectory + fileToMove.ToString(), destDirectory + fileToMove.ToString());
+
+                    filesMoved++;
                 }
-                catch (Exception)
-                {
-                    MessageBox.Show("Error!");
-                    throw;
-                }
-
-
-              //  var filesToMove = Directory.EnumerateFiles(sourceDir, "*.*")
-           // .Where(s => s.EndsWith(filetype, StringComparison.OrdinalIgnoreCase));
-
-               // string[] filesArray = filesToMove.ToArray<string>();
-               // MessageBox.Show(filesToMove.ToString());
-            
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error!");
+                return;
+                //throw;
+            }
+            MessageBox.Show("Successfully moved " + filesMoved.ToString() + " files!");
         }
 
         private void button_Execute_Click(object sender, RoutedEventArgs e)
@@ -79,6 +74,30 @@ namespace Filesharp
             else
             {
                 // sort()
+            }
+        }
+
+        private void comboBox1SelChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        private void cb1_dropDownClosed(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == move)
+            {
+                textbox1.Text = "Source directory (e.g., C:\\1\\)";
+                textbox2.Text = "Destination directory (e.g., C:\\2\\";
+                textbox3.Text = "Filetype to move (e.g., .png)";
+
+                hideControl(textbox4);
+            }
+            if (comboBox1.SelectedIndex == delete)
+            {
+                textbox1.Text = "Directory to delete files from (e.g., C:\\1\\)";
+                textbox2.Text = "Filetype to delete (e.g., .png)";
+
+                hideControl(textbox3);
+                hideControl(textbox4);
             }
         }
     }
